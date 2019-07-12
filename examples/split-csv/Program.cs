7a753efac1f427be21f5c9103ee923804f145e0c
Ed Elliott <goeddieuk@gmail.com>
2019-07-12 17:24:15 +0100

    read and write csv

---
+++ examples/split-csv/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Spark.Sql;
+using static Microsoft.Spark.Sql.Functions;
+
+namespace split_csv
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Spark!");
+
+            var spark = SparkSession
+                .Builder()
+                .GetOrCreate();
+
+            //Read a single CSV file
+            var source = spark
+                            .Read()
+                            .Option("header", true)
+                            .Option("inferShchema", true)
+                            .Option("ignoreLeadingWhiteSpace", true)
+                            .Option("ignoreTrailingWhiteSpace", true)
+                            .Csv("./source.csv");
+
+            //Write that CSV into many different CSV files, partitioned by city
+            source.Write()
+                    .Mode(SaveMode.Overwrite)
+                    .Option("header", true)
+                    .PartitionBy("city")
+                    .Csv("./output-partitioned-city-csv");
+
+        }
+    }
+}
