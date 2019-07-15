a6db8f124aa16d672dd20f0043f05fc01907e563
Ed Elliott <goeddieuk@gmail.com>
2019-07-15 07:41:47 +0100

    removing udf from csv example

--- examples/split-csv/Program.cs
+++ examples/split-csv/Program.cs
@@ -44,69 +44,9 @@ namespace split_csv
             
             partitions.PrintSchema();
 
-            // create a UDF that we will use to put peoples ages into groups, in the udf's type spec (Udf<int, int>) the last value is the return type and everything before the types of any parameters
-            // here we have one parameter (int) and return an int
-            // I have named the parameter age
-            var ageRange = Udf<int, int>((age) => {
-                //Write whatever code you want here, it will be awesome.
-                if(age < 30){
-                    return 1;
-                }
+            //Write the partitiond csv's back out as a single partition
 
-                if (age < 40){
-                    return 2;
-                }
-
-                if( age < 67){
-                    return 3;
-                }
-
-                return 4;
-            });
-            
-            //This udf takes two values and returns an int (poor peter)
-            var ageRangeUnlessPeter = Udf<int, string, int>((age, name) => {
-                
-                if(name == "peter"){
-                    return 99;
-                }
-
-                if(age < 30){
-                    return 1;
-                }
-
-                if (age < 40){
-                    return 2;
-                }
-
-                if( age < 67){
-                    return 3;
-                }
-
-                return 4;
-            });
-
-            //select all columns and append our new ageBucket UDF - use Alias to rename the column otherwise it is some weird name like "Int32 <Main>b__0_1(System.String)(age)" 
-            var everything = partitions
-                                .Select(
-                                    partitions.Col("*"),                                                    //select all existing columns (could have said city or name etc)
-                                    ageRange(partitions.Col("age")).Alias("ageBucket"),                     //call ageRange UDF for each row passing in "age" then alias the new column to ageBucket
-                                    ageRangeUnlessPeter(partitions.Col("age"), partitions.Col("name"))      //call ageRangeUnlessPeter UDF passing in age and name
-                                        .Alias("ageBucketUnlessPeter")                                      //    alias the new column to ageBucketUnlessPeter
-                                );
-
-            everything.Show();
-
-            //write the data partitioned by the new ageBucket column
-            everything.Write()
-                            .Mode(SaveMode.Overwrite)
-                            .Option("header", true)
-                            .PartitionBy("ageBucket")
-                            .Csv("./output-partitioned-age-bucket");
-
-            //Now lets filter by the UDF!
-            partitions.Filter(ageRangeUnlessPeter(partitions.Col("age"), partitions.Col("name")) == 99).Show();
-
+            partitions.Write().Mode(SaveMode.Overwrite).Option("header", true).Csv("./output-single-partition-again");
         }
     }
 }
