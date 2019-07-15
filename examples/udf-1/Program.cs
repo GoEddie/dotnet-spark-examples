2f5b90336e2adf27b5a43e45394a476e7e12caf6
Ed Elliott <goeddieuk@gmail.com>
2019-07-15 07:33:50 +0100

    csv and udf-1 example

--- examples/udf-1/Program.cs
+++ examples/udf-1/Program.cs
@@ -2,7 +2,7 @@
 using Microsoft.Spark.Sql;
 using static Microsoft.Spark.Sql.Functions;
 
-namespace split_csv
+namespace udf
 {
     class Program
     {        
@@ -15,32 +15,14 @@ namespace split_csv
                 .GetOrCreate();
 
             //Read a single CSV file
-            var source = spark
-                            .Read()
-                            .Option("header", true)
-                            .Option("inferShchema", true)
-                            .Option("ignoreLeadingWhiteSpace", true)
-                            .Option("ignoreTrailingWhiteSpace", true)
-                            .Csv("./source.csv");
-
-            //Write that CSV into many different CSV files, partitioned by city
-            source.Write()
-                    .Mode(SaveMode.Overwrite)
-                    .Option("header", true)
-                    .PartitionBy("city")
-                    .Csv("./output-partitioned-city-csv");
-
-            //Read the partitioned csv's back in
             // add in a more specific schema, we know what we want the types to be
-            // note - output-partitioned-city-csv is the parent directory with sub-folders for each city and they will
-            // all be found an read in, awesome!
             var partitions = spark
                             .Read()
                             .Option("header", true)
                             .Option("ignoreLeadingWhiteSpace", true)
                             .Option("ignoreTrailingWhiteSpace", true)                            
                             .Schema("name string, age int, city string") //instead of inferSchema we specify the schema
-                            .Csv("./output-partitioned-city-csv");
+                            .Csv("./source.csv");
             
             partitions.PrintSchema();
 
@@ -110,3 +92,5 @@ namespace split_csv
         }
     }
 }
+
+
